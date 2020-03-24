de8e33d43c2ef88ff4ce6b137e3e26a66da4c286
Capitan Empanada <ivanlp10n2@gmail.com>
2020-03-24 20:13:12 -0300

    finished firstMoveTests

--- src/test/java/com/empanada/tdd/chess/controller/FirstMoveTest.java
+++ src/test/java/com/empanada/tdd/chess/controller/FirstMoveTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 
-import com.empanada.tdd.chess.manager.Manager;
-import com.empanada.tdd.chess.manager.impl.ChessManager;
+import com.empanada.tdd.chess.components.Manager;
+import com.empanada.tdd.chess.components.impl.ChessManager;
 import com.empanada.tdd.chess.messaging.Command;
 import com.empanada.tdd.chess.messaging.Position;
 import com.empanada.tdd.chess.shared.CommandResponse;
@@ -20,6 +20,7 @@ public class FirstMoveTest {
   Manager manager;
 
   private final int SUCCESSFUL_STATUS_CODE = 200;
+  private final int FAIL_STATUS_CODE = 400;
 
   @BeforeEach
   public void startup() {
@@ -36,12 +37,36 @@ public class FirstMoveTest {
   public void movePieceOK() {
     final Position pawn = Position.of('D', '2');
     final Position destiny = Position.of('D', '3');
-    final Command command = Command.of(pawn, destiny);
+    final Command validMovement = Command.of(pawn, destiny);
 
-    final ResponseEntity<CommandResponse> response = chessAPI.move(command);
+    final ResponseEntity<CommandResponse> response = chessAPI.move(validMovement);
 
     assertEquals(response.getStatusCode(), SUCCESSFUL_STATUS_CODE);
     assertEquals(response.getStatusCode(), "Todo ok");
+  }
+
+  @Test
+  public void invalidMove() {
+    final Position bishop = Position.of('F', '8');
+    final Position destiny = Position.of('D', '3');
+    final Command invalidMovement = Command.of(bishop, destiny);
+
+    final ResponseEntity<CommandResponse> response = chessAPI.move(invalidMovement);
+
+    assertEquals(response.getStatusCode(), FAIL_STATUS_CODE);
+    assertEquals(response.getStatusCode(), "invalid move");
+  }
+
+  @Test
+  public void outOfBoundMove() {
+    final Position bishop = Position.of('F', '8');
+    final Position destiny = Position.of('D', '9');
+    final Command invalidMovement = Command.of(bishop, destiny);
+
+    final ResponseEntity<CommandResponse> response = chessAPI.move(invalidMovement);
+
+    assertEquals(response.getStatusCode(), FAIL_STATUS_CODE);
+    assertEquals(response.getStatusCode(), "out of bounds");
   }
 
 }

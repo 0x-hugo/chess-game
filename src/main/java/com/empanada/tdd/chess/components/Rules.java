9ea19a9aa9db13e8c93376f773a458d8718af926
Capitan Empanada <ivanlp10n2@gmail.com>
2020-03-28 14:56:28 -0300

    rules COR

--- src/main/java/com/empanada/tdd/chess/components/Rules.java
+++
@@ -1,9 +0,0 @@
-package com.empanada.tdd.chess.components;
-
-/**
- * it validates the play. It does not contain information about pieces location
- * and state of the game
- */
-public interface Rules {
-
-}
